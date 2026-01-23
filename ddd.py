ff4759d7f2d4050dda2e90853a16c579520bc5c6
Naveen142005 <valarmathi43679@gmail.com>
2026-01-23 14:09:50 +0530

    Initial commit

---
+++ ddd.py
@@ -0,0 +1,16 @@
+class naveen:
+    def hello(self):
+        pass
+
+class kumar:
+    def hello(self):
+        pass
+
+class maith:
+    def hello(self):pass
+
+ma = naveen()
+ma.hello()
+
+k = kumar()
+k.hello
